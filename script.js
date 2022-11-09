a17c3f3f7c6781506a2aa8c85875b67843aa0797
Juliette <juliette.durel@outlook.fr>
2022-11-09 12:03:27 +0100

    miseàjour

---
+++ script.js
@@ -0,0 +1,35 @@
+// branchement route search
+
+const searchButton = document.getElementById('search-button')
+const searchDeparture = document.getElementById('search-departure')
+const searchArrival = document.getElementById('search-arrival')
+const searchDate = document.getElementById('search-date')
+
+searchButton.addEventListener('click', () => {
+    const departure = searchDeparture.value
+    const arrival = searchArrival.value
+    const date = searchDate.value
+    //note : date format 2022-11-22
+
+    const searchResult = fetch(`http://localhost:3000/trips/${departure}&${arrival}&${date}`)
+        .then(response => response.json())
+        .then(data => {
+            console.log(data.trips);
+            for (const trip of data.trips) {
+                const arrival = trip.arrival
+                const date = new Date(trip.date)
+                const departure = trip.departure
+                const price = trip.price
+                document.getElementById('search-result').innerHTML += `
+                <div class = 'trip'> 
+                    <p>${arrival}>${departure}</p>
+                    <p>${date.getHours()}:${date.getMinutes()}</p>
+                    <p>${price}€</p>
+                    <button class='book'>Book</button>
+                </div>       
+       `
+            }
+        });
+
+
+})
